9c92aace4e7a06aba4a1aa382c9e1f532e1e0648
NorthHuang <hzl136133@gmail.com>
2024-09-26 22:51:25 +1000

    analysis api

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # report-backend-zile
 network analysis report backend code for TIP of Swinburne
+
+## Prerequisites
+
+- Python 3.x installed
+- MySQL server running locally or on a remote server
+- `pip3` installed for managing Python packages
+
+## Steps to Set Up and Run the Project
+
+### 1. Install Dependencies
+
+Before starting, install all the required dependencies using `pip`. Run the following command:
+
+```bash
+pip3 install -r requirements.txt
+```
+
+### 2. Setup your own DB config(using mysql)
+Copy database.yml.example to database.yml
+
+Make sure to update the values according to your MySQL setup:
+
+host: the hostname or IP address where MySQL is running (usually localhost for local development)
+port: the port MySQL is listening on (usually 3306)
+username: your MySQL username
+password: your MySQL password
+database: the database name you are using
+
+### 3. Run setup script to be ready
+```
+python3 scripts/setup.py
+```
+
+### 4. Start service
+```
+python3 -m flask run --debug
+```
---
+++ scripts/setup.py
@@ -0,0 +1,39 @@
+import mysql.connector
+import yaml
+import os
+
+def create_results_directory():
+    if not os.path.exists('results'):
+        os.makedirs('results')
+        print("Created 'results' directory.")
+    else:
+        print("'results' directory already exists.")
+
+def load_database_config():
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    config_path = os.path.join(script_dir, '../database.yml')
+    with open(config_path, 'r') as f:
+        config = yaml.safe_load(f)
+    return config
+
+def create_table():
+    print("start creating questions table..........")
+    config = load_database_config()
+    connection = mysql.connector.connect(**config['development'])  # Change to appropriate environment
+    cursor = connection.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS analysis_results (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            result_file_url VARCHAR(255) NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+        )
+    """)
+    cursor.close()
+    connection.close()
+    print("creating questions table successfully..........")
+
+if __name__ == "__main__":
+    create_results_directory()
+
+    create_table()
